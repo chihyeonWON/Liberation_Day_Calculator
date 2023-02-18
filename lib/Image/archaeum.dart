1921e698c5eee474ed7933105696b4d4ea48c20c
ylose <heungeob1003@gmail.com>
2023-02-19 03:31:11 +0900

    아카이럼 격파 조건 다이얼로그 추가

--- lib/Image/archaeum.dart
+++ lib/Image/archaeum.dart
@@ -1,20 +1,76 @@
 import 'package:flutter/material.dart';
 
-class ArchaeumImage extends StatelessWidget {
+class ArchaeumImage extends StatefulWidget {
   const ArchaeumImage({Key? key}) : super(key: key);
+
+  @override
+  State<ArchaeumImage> createState() => _ArchaeumImageState();
+}
+
+class _ArchaeumImageState extends State<ArchaeumImage> {
+
+  /* Dialog 띄우기 */
+  void showDialogPop() {
+    showDialog(
+      context: context,
+      barrierDismissible:
+      false, //다이얼로그 바깥을 터치 시에 닫히도록 하는지 여부 (true: 닫힘, false: 닫히지않음)
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            //제목 정의
+            '군단장 아카이럼',
+          ),
+          content: SingleChildScrollView(
+            //내용 정의
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  '봉인된 제네시스 무기와 보조무기만 장착',
+                ),
+                Text(
+                  '착용 중인 장비의 순수 능력치만 적용',
+                ),
+                Text(
+                  '최종 데미지 75% 감소',
+                ),
+                Text(
+                  '혼자서 격파',
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            //버튼 정의
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 현재 화면을 종료하고 이전 화면으로 돌아가기
+              },
+              child: Text(
+                '취소',
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Center(
-          child:
-          Image.asset(
-            'asset/img/archaeum.png',
+          child: InkWell(
+            onTap:showDialogPop,
+            child: Image.asset(
+              'asset/img/archaeum.png',
 
-            // 화면의 반만큼 높이 구현
-            height: MediaQuery.of(context).size.height/2,
+              // 화면의 반만큼 높이 구현
+              height: MediaQuery.of(context).size.height/2,
+            ),
           )
       ),
     );
   }
 }
+
