2d2b441054331d7488f48d8f096b565248842c64
ylose <heungeob1003@gmail.com>
2023-02-19 03:31:52 +0900

    데미안 격파 조건 다이얼로그 추가

--- lib/Image/demian.dart
+++ lib/Image/demian.dart
@@ -1,20 +1,70 @@
 import 'package:flutter/material.dart';
 
-class DemianImage extends StatelessWidget {
+class DemianImage extends StatefulWidget {
   const DemianImage({Key? key}) : super(key: key);
+
+  @override
+  State<DemianImage> createState() => _DemianImageState();
+}
+
+class _DemianImageState extends State<DemianImage> {
+
+  /* Dialog 띄우기 */
+  void showDialogPop() {
+    showDialog(
+      context: context,
+      barrierDismissible:
+      false, //다이얼로그 바깥을 터치 시에 닫히도록 하는지 여부 (true: 닫힘, false: 닫히지않음)
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            //제목 정의
+            '군단장 데미안',
+          ),
+          content: SingleChildScrollView(
+            //내용 정의
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  '데스카운트 10개->5개로 감소',
+                ),
+                Text(
+                  '혼자서 격파',
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            //버튼 정의
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 현재 화면을 종료하고 이전 화면으로 돌아가기
+              },
+              child: Text(
+                '취소',
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Center(
-          child:
-          Image.asset(
-            'asset/img/demian.png',
+          child: InkWell(
+            onTap:showDialogPop,
+            child: Image.asset(
+              'asset/img/demian.png',
 
-            // 화면의 반만큼 높이 구현
-            height: MediaQuery.of(context).size.height/2,
+              // 화면의 반만큼 높이 구현
+              height: MediaQuery.of(context).size.height/2,
+            ),
           )
       ),
     );
   }
 }
+
