d79c4e19b85ecb8ab1045566cb82fea9d69bdf53
ylose <heungeob1003@gmail.com>
2023-02-19 02:08:33 +0900

    진힐라 이미지 추가

---
+++ lib/Image/jinheila.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class JinheilaImage extends StatelessWidget {
+  const JinheilaImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+          child:
+          Image.asset(
+            'asset/img/jinheila.png',
+
+            // 화면의 반만큼 높이 구현
+            height: MediaQuery.of(context).size.height/2,
+          )
+      ),
+    );
+  }
+}
