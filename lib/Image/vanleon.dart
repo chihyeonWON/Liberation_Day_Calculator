74f08578d0bc3dbbb324ed079e0cfe6e3e1b9d11
ylose <heungeob1003@gmail.com>
2023-02-19 03:08:35 +0900

    반레온 격파 조건 다이얼로그 추가

--- lib/Image/vanleon.dart
+++ lib/Image/vanleon.dart
@@ -69,7 +69,7 @@ class _VanleonImageState extends State<VanleonImage> {
               // 화면의 반만큼 높이 구현
               height: MediaQuery.of(context).size.height/2,
             ),
-          )
+          ),
       ),
     );
   }
