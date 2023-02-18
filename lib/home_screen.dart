8fc13fbd6107cc38f804c9d66861094874c2b653
ylose <heungeob1003@gmail.com>
2023-02-18 15:25:36 +0900

    검은색 배경으로 변경

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -45,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[100], // 핑크 배경색 적용
+      backgroundColor: Colors.black, // 검은색 배경색 적용
       body: SafeArea( // 시스템 UI 피해서 UI 그리기
         top: true,
         bottom:false,
