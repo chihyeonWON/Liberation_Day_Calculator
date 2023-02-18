fbb579944f20c557d9b34dd62a13d6413f2fd3a0
ylose <heungeob1003@gmail.com>
2023-02-19 02:08:03 +0900

    데미안 이미지 추가

---
+++ lib/Image/demian.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DemianImage extends StatelessWidget {
+  const DemianImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+          child:
+          Image.asset(
+            'asset/img/demian.png',
+
+            // 화면의 반만큼 높이 구현
+            height: MediaQuery.of(context).size.height/2,
+          )
+      ),
+    );
+  }
+}
